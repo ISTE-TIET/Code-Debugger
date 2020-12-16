b344141ec8d1c531b90fe6c336706a5be5ff3be9
rishusingh022 <rishusingh022@gmail.com>
2020-12-16 20:05:41 +0530

    resolved all bugs

--- demo.c
+++ demo.c
@@ -1,9 +1,8 @@
-
 #include <stdio.h>
 
 void main(){
-    int i=0
-    for(printf("hello world");||i<10&&;i++){
-        printf("hello world")
+    int i=0;
+    for(printf("hello world");i<10;i++){
+        printf("hello world");
     }
 }
