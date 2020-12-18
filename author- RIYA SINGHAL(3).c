c608a1546f17d44ac6cf06352e64745d896d4e28
samgarg7009 <samikshagarg321@gmail.com>
2020-12-18 21:40:38 +0530

    RIYA SINGHAL(3) :: adjust size of output

--- author- RIYA SINGHAL(3).c
+++ author- RIYA SINGHAL(3).c
@@ -1,14 +1,13 @@
-
 #include <stdio.h>
 
 int main()
 {
     int x=10;
-    for(int i=0;i<5;i-=1)
+    for(int i=0;i<5;i+=1)
     {
         int x=9;
-        printf("the value of x variable is %d",x);
-        
+        printf("the value of x variable is %d\n",x);
+
     }
     return 0;
 }
