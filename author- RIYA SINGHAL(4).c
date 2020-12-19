b1bad0d541e08aa7359942b4b6bd85f123cd4d4b
samgarg7009 <samikshagarg321@gmail.com>
2020-12-19 13:10:55 +0530

    RIYA SINGHAL(4) :: conflicting type

--- author- RIYA SINGHAL(4).c
+++ author- RIYA SINGHAL(4).c
@@ -3,16 +3,16 @@
 
 int main()
 {
-    int x=10;
-    for(int i=0;i<5;i-=1)//INFINITE LOOP
+    int x = 10;
+    for(int i=0;i<5;i+=1)//INFINITE LOOP
     {
         int x=9;
-        printf("the value of x variable is %d",x);
-        
+        printf("the value of x variable is %d\n",x);
+
     }
-    char x;//ALREADY DECLARED ABOVE
-    printf("enter any character");
-    scanf("%c",&x);
-    printf("the entered character is %c",x);
+    char ch;//ALREADY DECLARED ABOVE
+    printf("enter any character ");
+    scanf("%c",&ch);
+    printf("the entered character is %c",ch);
     return 0;
 }
